064b76826029829fd74e1bacbc6920ee1b47fa61
FurkannByrm <frknbyrm05@gmail.com>
2026-01-22 12:14:10 +0300

    dots

---
+++ init.lua
@@ -0,0 +1,14 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Temel ayarlar (Satır numaraları, tab genişliği vb.)
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
+require("lazy").setup("plugins")
